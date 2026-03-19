19c9489fb90cb50ff975e97b4e61d897b5304406
gabriel.bueno-ext <gabriel.bueno-ext@betchavas.com>
2026-03-19 14:19:59 -0300

    [feat] JWT token and auth middleware implemantation

---
+++ src/Application/Middleware/Auth.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Application\Middleware;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\ExpiredException;
+
+class Auth {
+    public static function handle()
+    {
+        $headers = getallheaders();
+
+        if(!isset($headers['Authorization'])) {
+            http_response_code(401);
+            exit('invalid token');
+        }
+
+        $token = str_replace('Bearer ', '', $headers['Authorization']);
+
+        try {
+            $decode = JWT::decode(
+                $token,
+                new Key(getenv('JWT_SECRET_KEY'),'HS256')
+            );
+
+            return $decode;
+        } catch (ExpiredException $e) {
+            http_response_code(401);
+            exit("Token inválido ou expirado: {$e}");
+        }
+    }
+}
---
+++ src/Domain/Services/Jwt/JwtGenerate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Domain\Services\Jwt;
+use Firebase\JWT\JWT;
+use Exception;
+
+
+class JwtGenerate {
+    public static function generate($id)
+    {
+        try {
+            $payload = [
+                "iss" => getenv("JWT_ISS"),
+                "iat" => time(),
+                "exp" => time() + 420,
+                "sub" => $id,
+            ];
+    
+            $jwt =[
+                'token' => JWT::encode($payload,getenv('JWT_SECRET_KEY'),'HS256')
+            ];
+    
+            return $jwt;
+        } catch (\Exception $e) {
+            throw new Exception("Error Processing Request", 1);
+        }
+    }
+}
